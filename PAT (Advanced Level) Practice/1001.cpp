3484c174ebe104864100507b983565648a1df875
shzxpy <494302110@qq.com>
2019-02-24 16:01:06 +0800

    Update 1001.cpp

--- PAT (Advanced Level) Practice/1001.cpp
+++ PAT (Advanced Level) Practice/1001.cpp
@@ -5,20 +5,20 @@ int main(){
 	int i=0,j;
 	scanf("%d%d",&a,&b);
 	c=a+b;
-	if(c<0){
+	if(c<0){ //判断正负 
 		printf("-");
 		c=-c;		
 	}
 	if(c==0){
 		arr[i++]=0;
 	}
-	while(c!=0){
+	while(c!=0){//从各位把位数传入数组 
 		arr[i++]=c%10;
 		c=c/10;
 	} 
-	for(j=i-1;j>=0;j--){
+	for(j=i-1;j>=0;j--){//反向输出数组 
 		printf("%d",arr[j]);
-		if(j%3==0&&j>0){
+		if(j%3==0&&j>0){// 每三位添加"," 
 			printf(",");
 		}
 	}
